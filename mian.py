8ae8e7891cd6072355590c3beb3a254a7140e43b
MichalKazik <kazmierczakmichal14@gmail.com>
2024-10-29 19:35:17 +0100

    Update mian.py

--- mian.py
+++ mian.py
@@ -1,1 +1,7 @@
-print('hello world')
+meme_dict = {
+            "CRINGE": "Coś wyjątkowo dziwnego lub zawstydzającego",
+            "LOL": "Częsta reakcja na coś zabawnego",
+            "REEl":"Mam tak samo/cuje sie tak samo"
+            "SIGMA":"Jestes COOL/Jestes madry"
+            "AURA":"AALE FAJNIE/ALE DO DUPY"
+            }
